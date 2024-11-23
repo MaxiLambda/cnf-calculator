ec69556c2cac4148bb4f5fa69aba0b43f248e8fd
Maxi <maximilian.lincks@gmx.net>
2024-11-23 17:57:20 +0100

    Tseitin works

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,13 +10,29 @@ public class App
 {
     public static void main( String[] args )
     {
-       String input = "(!x1 && !(x3 <> x2)) || ((x3 => !x4) && (x1 => (x2 || !x3)) && x4))";
+//       String input = "(!x1 && !(x3 <> x2)) || ((x3 => !x4) && (x1 => (x2 || !x3)) && x4))";
+       String input = "(x && !y) || (z || (x && !w))";
        var lexer = LexerSetup.lexer;
        var parser = ParserSetup.parser;
        var simplificationInterpreter = SimplificationInterpreterSetup.interpreter;
-
-       AstExpression<?> res  = simplificationInterpreter.run(lexer, parser, input);
+       var tseitinInterpreter = TseitinInterpreterSetup.interpreter;
+//
+       var res  = simplificationInterpreter.run(lexer, parser, input);
         System.out.println(AstPrinter.printString(res));
+        AstExpression<?> cnf = tseitinInterpreter.run((AstExpression<AstExpression<?>>) res);
+        System.out.print(AstPrinter.printString(cnf));
+        System.out.println(" && y" + (TseitinInterpreterSetup.getCounter() -1));
+
+
+//        AstExpression<?> cnf = tseitinInterpreter.run(lexer, parser, input);
+//
+//        System.out.print(AstPrinter.printString(cnf));
+//        System.out.println(" && y" + (TseitinInterpreterSetup.getCounter() -1));
+//        System.out.println("--");
+//        System.out.println("(!y0 || x) && (!y0 || !y) && (y0 || !x || !!y) && (!y1 || x) && (!y1 || !w) && (y1 || !x || !!w) && (y2 || !z) && (y2 || !y3) && (!y2 || z || y3) && (y4 || !y5) && (y4 || !y6) && (!y4 || y5 || y6) && y6");
+
     }
+
+
 
 }
--- src/main/java/org/example/TseitinInterpreterSetup.java
+++ src/main/java/org/example/TseitinInterpreterSetup.java
@@ -1,21 +1,28 @@
 package org.example;
 
 import lincks.maximilian.impl.monad.MList;
+import lincks.maximilian.impl.monad.Maybe;
 import lincks.maximilian.parser.Interpreter;
 import lincks.maximilian.parser.parser.ast.*;
+import lincks.maximilian.parser.token.Symbol;
+import lombok.Getter;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.example.Symbols.*;
-import static org.example.Symbols.intersection;
 
 public class TseitinInterpreterSetup {
-    //Removes <> and => and propagates !
     private static final Context<AstExpression<?>> context = new Context<>(Map.of(
             negation, TseitinInterpreterSetup::negation,
             union, TseitinInterpreterSetup::union,
             intersection, TseitinInterpreterSetup::intersection
     ));
+
+    //TODO this needs resting after every run -> don't use static setup
+    @Getter
+    private static int counter = 0;
+    public static final HashMap<String, Integer> newFormulaNr = new HashMap<>();
 
     public static final Interpreter<AstExpression<?>> interpreter = new Interpreter<>(TseitinInterpreterSetup::fromLiteral, context);
 
@@ -32,27 +39,12 @@ public class TseitinInterpreterSetup {
 
     private static AstExpression<?> neg(AstExpression<?> expr) {
         switch (expr) {
-            case ValueLiteral<?> v -> {
-                //negate the value by returning a negated expression - RECURSIVE
-                return neg((AstExpression<?>) v.getValue());
-            }
             case SymbolLiteral<?> v -> {
                 //negate by returning new Expression with negation operator
                 return new Expression<>(negation, new MList<>(v));
             }
-            case Expression<?> v -> {
-                if (v.getSymbol().equals(negation)) {
-                    //remove negation by just returning the first argument of the negation
-                    return v.getArgs().head();
-                } else if (v.getSymbol().equals(union)) {
-                    //!(a || b) -> (!a && !b)
-                    return new Expression<>(intersection, v.getArgs().map(TseitinInterpreterSetup::neg).map(e -> (AstExpression<AstExpression<?>>) e));
-                } else if (v.getSymbol().equals(intersection)) {
-                    //!(a && b) -> (!a || !b)
-                    return new Expression<>(union, v.getArgs().map(TseitinInterpreterSetup::neg).map(e -> (AstExpression<AstExpression<?>>) e));
-                }
-                throw new RuntimeException();
-            }
+            //real Seitin needs this, but if SimplificationInterpreter is run before, only SymbolLiterals are negated
+            default -> throw new RuntimeException();
         }
     }
 
@@ -62,12 +54,107 @@ public class TseitinInterpreterSetup {
     }
 
     private static Literal<AstExpression<?>> union(MList<Literal<AstExpression<?>>> l) {
-        var params = l.map(TseitinInterpreterSetup::fromLiteral).map(e -> (AstExpression<Object>) e);
-        return new ValueLiteral<>(new Expression<>(union, params));
+        var params = l.map(TseitinInterpreterSetup::fromLiteral);
+        var G = l.head();
+        var H = l.tail().head();
+
+        var paramsAst = params.map(e -> (AstExpression<AstExpression<?>>) e);
+
+        Maybe<AstExpression<AstExpression<?>>> seitinRoot = Maybe.nothing();
+
+        AstExpression<AstExpression<?>> F = new Expression<>(union, paramsAst);
+        int fNr = newFormulaNr.computeIfAbsent(AstPrinter.printString(F), x -> counter++);
+
+        AstExpression<AstExpression<?>> h;
+        AstExpression<AstExpression<?>> g;
+
+        if (isLiteral(G)) {
+            g = G;
+        } else {
+            seitinRoot = new Maybe<>(G);
+            int gNr = newFormulaNr.computeIfAbsent(AstPrinter.printString(G), x -> counter++);
+            g = new SymbolLiteral<>(new Symbol("y" + gNr));
+        }
+        if (isLiteral(H)) {
+            h = H;
+        } else {
+            seitinRoot = new Maybe<>(seitinRoot.<AstExpression<AstExpression<?>>>map(ignore -> new Expression<>(intersection, paramsAst)).otherwise(H));
+            int hNr = newFormulaNr.computeIfAbsent(AstPrinter.printString(H), x -> counter++);
+            h = new SymbolLiteral<>(new Symbol("y" + hNr));
+        }
+
+        return seitinReplacement(fNr, seitinRoot, g, h);
     }
 
     private static Literal<AstExpression<?>> intersection(MList<Literal<AstExpression<?>>> l) {
-        var params = l.map(TseitinInterpreterSetup::fromLiteral).map(e -> (AstExpression<Object>) e);
-        return new ValueLiteral<>(new Expression<>(intersection, params));
+        var params = l.map(TseitinInterpreterSetup::fromLiteral);
+        var G = l.head();
+        var H = l.tail().head();
+
+        var paramsAst = params.map(e -> (AstExpression<AstExpression<?>>) e);
+
+        Maybe<AstExpression<AstExpression<?>>> seitinRoot = Maybe.nothing();
+
+        AstExpression<AstExpression<?>> F = new Expression<>(intersection, paramsAst);
+        int fNr = newFormulaNr.computeIfAbsent(AstPrinter.printString(F), x -> counter++);
+
+        AstExpression<AstExpression<?>> h;
+        AstExpression<AstExpression<?>> g;
+
+        if (isLiteral(G)) {
+            g = G;
+        } else {
+            seitinRoot = new Maybe<>(G);
+            int gNr = newFormulaNr.computeIfAbsent(AstPrinter.printString(G), x -> counter++);
+            g = new SymbolLiteral<>(new Symbol("y" + gNr));
+        }
+        if (isLiteral(H)) {
+            h = H;
+        } else {
+            seitinRoot = new Maybe<>(seitinRoot.map(ignore -> F).otherwise(H));
+            int hNr = newFormulaNr.computeIfAbsent(AstPrinter.printString(H), x -> counter++);
+            h = new SymbolLiteral<>(new Symbol("y" + hNr));
+        }
+
+        return seitinReplacement(fNr, seitinRoot, g, h);
+    }
+
+    private static Literal<AstExpression<?>> seitinReplacement(int fNr, Maybe<AstExpression<AstExpression<?>>> seitinRoot, AstExpression<AstExpression<?>>g, AstExpression<AstExpression<?>> h){
+        AstExpression<AstExpression<?>> f = new SymbolLiteral<>(new Symbol("y" + fNr));
+        AstExpression<AstExpression<?>> fNot = (AstExpression<AstExpression<?>>)SimplificationInterpreterSetup.interpreter.run(new Expression<>(negation, new MList<>(f)));
+        AstExpression<AstExpression<?>> gNot = (AstExpression<AstExpression<?>>)SimplificationInterpreterSetup.interpreter.run(new Expression<>(negation, new MList<>(g)));
+        AstExpression<AstExpression<?>> hNot = (AstExpression<AstExpression<?>>) SimplificationInterpreterSetup.interpreter.run(new Expression<>(negation, new MList<>(h)));
+
+        var gNotAndhNot = new Expression<>(union, new MList<>(gNot, hNot));
+
+        var litE1 = new Expression<>(union, new MList<>(fNot, g));
+        var litE2 = new Expression<>(union, new MList<>(fNot, h));
+        var litE3 = new Expression<>(union, new MList<>(f, gNotAndhNot));
+
+        var litE4 = new Expression<>(intersection, new MList<>(litE1, litE2));
+        var tseitinF = new Expression<>(intersection, new MList<>(litE4, litE3));
+
+        Literal<AstExpression<?>> res = new ValueLiteral<>(
+                seitinRoot.map(root -> new Expression<>(
+                        intersection,
+                        new MList<>(root, tseitinF))).otherwise(tseitinF));
+
+        newFormulaNr.put(AstPrinter.printString(res),fNr);
+
+        return res;
+    }
+
+    private static  boolean isLiteral(AstExpression<?> expression) {
+        switch (expression) {
+            case Expression<?> v -> {
+                return v.getSymbol().equals(negation) && isLiteral(v.getArgs().head());
+            }
+            case ValueLiteral<?> v -> {
+                return isLiteral((AstExpression<?>) v.getValue());
+            }
+            case SymbolLiteral<?> v -> {
+                return true;
+            }
+        }
     }
 }
